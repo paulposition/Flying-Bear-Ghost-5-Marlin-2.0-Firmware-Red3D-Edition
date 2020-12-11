995840a6b49ea0984a4852ad2bdc8e93b3cc48be
Simone Carnio <simone.carnio@foolix.com>
2020-12-11 19:24:26 +0100

    customized remaining graphics element

--- buildroot/share/PlatformIO/scripts/copy_custom_assets.py
+++ buildroot/share/PlatformIO/scripts/copy_custom_assets.py
@@ -5,7 +5,6 @@ import shutil
 custom_graphics_toolpath = os.path.join(env.Dictionary("PROJECT_DIR"), "custom_graphics") + "/"
 custom_graphics_path = os.path.join(env.Dictionary("PROJECT_DIR"), "custom_graphics", "images") + "/"
 custom_fonts_path = os.path.join(env.Dictionary("PROJECT_DIR"), "custom_graphics", "fonts") + "/"
-converted_assets_path = os.path.join(env.Dictionary("PROJECT_DIR"), "custom_graphics", "converted") + "/"
 assets_path = os.path.join(env.Dictionary("PROJECT_BUILD_DIR"), env.Dictionary("PIOENV"), "assets")
 
 def convert_assets():
@@ -24,13 +23,6 @@ def copy_fonts():
 			print("-> " + filename )
 			shutil.copy(os.path.join(custom_fonts_path, filename), assets_path)	
 
-def copy_converted_assets():
-	print("*** Copying Converted Assets *** ")
-	for filename in os.listdir(converted_assets_path):
-		if ".bin" in filename:	
-			print("-> " + filename )
-			shutil.copy(os.path.join(converted_assets_path, filename), assets_path)	
-
 
 
 shutil.rmtree(assets_path, ignore_errors=True)
@@ -39,5 +31,4 @@ if os.path.exists(assets_path) == False:
 
 convert_assets()
 copy_fonts()
-copy_converted_assets()
 
